4a56b69e55f95704c0ff0555459da45c5628ba55
Mikhail Chumakov <chumakovmedia@gmail.com>
2019-05-28 13:48:16 +0300

    remove settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-PROXY = {'proxy_url': 'socks5h://t3.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}} # 5 настройки прокси. Консанты прнято писать капсом 'PROXY'
-
-
-API_KEY = "640766054:AAElGpwr1a5IAHjhisA9bgrbtE5PDLX9tfI"
